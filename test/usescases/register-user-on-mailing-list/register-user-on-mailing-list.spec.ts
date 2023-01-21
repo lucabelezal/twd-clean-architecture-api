4378bc479719bcb790ffc3d546556eb1f16cfdd9
Lucas Nascimento <lucabelezal@gmail.com>
2023-01-21 18:22:18 -0300

    feat: add user data

---
+++ test/usescases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,10 @@
+import { UserData } from '@/entities/user-data'
+
+describe('Register user on mailing list use case', () => {
+  test('should add user with complete data to mailing list', () => {
+    const users: UserData[] = []
+    console.log(users)
+  })
+})
+
+// Arrange, Act, Assert
