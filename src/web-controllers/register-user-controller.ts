3b9d784177a6470578d32311a32d117b99f061a8
Lucas Nascimento <lucabelezal@gmail.com>
2023-02-09 19:17:52 -0300

    refactor: add index of MissingParamError

--- src/web-controllers/register-user-controller.ts
+++ src/web-controllers/register-user-controller.ts
@@ -2,7 +2,7 @@ import { UserData } from '@/entities'
 import { UseCase } from '@/usecases/ports'
 import { HttpRequest, HttpResponse } from '@/web-controllers/ports'
 import { badRequest, ok, serverError } from '@/web-controllers/util'
-import { MissingParamError } from './errors/missing-param-error'
+import { MissingParamError } from '@/web-controllers/errors'
 
 export class RegisterAndSendEmailController {
   private readonly usecase: UseCase
